a1bed3b46cdbfa12870a25c0a047b6b8d8d51315
Cory Williams <cory321@gmail.com>
2022-11-14 19:14:50 -0800

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-const App = () => {
+import './categories.styles.scss'
+import categories from './categories.json'
 
-const categories = [
-  {
-    id: 1,
-    title: 'Hats',
-  },
-  {
-    id: 2,
-    title: 'Jackets',
-  },
-  {
-    id: 3,
-    title: 'Sneakers',
-  },
-  {
-    id: 4,
-    title: 'Womens',
-  },
-  {
-    id: 5,
-    title: 'Mens',
-  }
-]
+const App = () => {
 
   return (
     <div className='categories-container'>
-    {categories.map(( {title} ) => (
-      <div className='category-container'>
-        {/* <img /> */}
+    {categories.map(( {title, id, imageUrl} ) => (
+      <div key={id} className='category-container'>
+        <div className='background-image' style={{
+          backgroundImage: `url(${imageUrl})`
+        }} />
         <div className='category-body-container'>
           <h2>{title}</h2>
           <p>Shop Now</p>
