711251530f64fc49fda534653300e0fc1b8f26c5
Cory Williams <cory321@gmail.com>
2022-11-15 14:20:53 -0800

    updated sign in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,39 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import { UserContext } from "../../contexts/user.context";
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(async () => {
+    const response = await getRedirectResult(auth);
+    if (response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user);
+    }
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect();
+    console.log(user);
   };
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign In With Google Redirect
+      </button>
     </div>
   );
 };
