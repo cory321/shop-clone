b09f4511b99b26b5a647ba53c762c57e0e5af7a6
Cory Williams <cory321@gmail.com>
2022-11-14 20:09:07 -0800

    reorganized hierarchy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import './categories.styles.scss'
-import categories from './categories.json'
+import Directory from "./components/directory/directory.component";
+import "./categories.styles.scss";
+import categories from "./categories.json";
 
 const App = () => {
-
-  return (
-    <div className='categories-container'>
-    {categories.map(( {title, id, imageUrl} ) => (
-      <div key={id} className='category-container'>
-        <div className='background-image' style={{
-          backgroundImage: `url(${imageUrl})`
-        }} />
-        <div className='category-body-container'>
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-  ))}
-
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
