f8978cb88e8dbec397449464d169f7855d8461d1
Cory Williams <cory321@gmail.com>
2022-11-15 11:16:12 -0800

    updated nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <h1>I am the navigation bar</h1>
-      <Outlet />
-    </div>
-  );
-};
+import Navigation from "./routes/home/navigation/navigation.component";
 
 const ShopPage = () => {
   return <div>I am the Shop Page</div>;
